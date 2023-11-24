bebb7014cd7906a2c83ef9d3898a7207494830cd
tubi55 <tubi55@nate.com>
2023-11-24 11:18:46 +0900

    footer styleing

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,5 +1,25 @@
 import './Footer.scss';
+import { FaFacebookF, FaTwitter, FaYoutube } from 'react-icons/fa';
+//npm i react-icons
 
 export default function Footer() {
-	return <div className='Footer'>Footer</div>;
+	return (
+		<footer className='Footer'>
+			<h1>Dcodelab</h1>
+
+			<p>2023 Dcodelab &copy; All Rights Reserved.</p>
+
+			<ul>
+				<li>
+					<FaFacebookF />
+				</li>
+				<li>
+					<FaTwitter />
+				</li>
+				<li>
+					<FaYoutube />
+				</li>
+			</ul>
+		</footer>
+	);
 }
