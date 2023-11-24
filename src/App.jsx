b66ca540813d0443b939f97a3ce313ae63de90f4
tubi55 <tubi55@nate.com>
2023-11-24 09:49:11 +0900

    모든 컴포넌트들 App에서 호출

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import Footer from './components/common/footer/Footer';
+import Header from './components/common/header/Header';
+import MainWrap from './components/main/mainWrap/MainWrap';
+import Members from './components/sub/members/Members';
+import Community from './components/sub/community/Community';
+import Contact from './components/sub/contact/Contact';
+import Department from './components/sub/department/Department';
+import Gallery from './components/sub/gallery/Gallery';
+import Youtube from './components/sub/youtube/Youtube';
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<Header />
+			<MainWrap />
+			<Department />
+			<Youtube />
+			<Gallery />
+			<Community />
+			<Members />
+			<Contact />
+			<Footer />
+		</>
 	);
 }
 
