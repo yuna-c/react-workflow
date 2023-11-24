8e480114cfc7d648e53ef5d4ee35fa8470904b7c
tubi55 <tubi55@nate.com>
2023-11-24 12:52:05 +0900

    커스텀훅의 특정 기능을 함수 묶어서 서로다른 컴포넌트에서 재활용하는 방법

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,10 +1,19 @@
 import { useEffect, useRef } from 'react';
 import './Layout.scss';
+import { useSplitText } from '../../../hooks/useSplitText';
 
 export default function Layout({ children, title }) {
 	const refFrame = useRef(null);
+	//useEffect안쪽에서 자주쓰일만한 특정 함수를 호출해야됨
+	//use로 시작하는 커스텀훅은 특정함수 안쪽에서 호출불가
+	//해당 hook이 함수를 반환하도록 처리
+	const splitText = useSplitText();
+	console.log(splitText);
 
 	useEffect(() => {
+		//아래처럼 custom hook이 반환한 함수를
+		//hook이나 핸들러함수 내부에서 사용가능
+		splitText('hello');
 		refFrame.current.classList.add('on');
 	}, []);
 
