ea5de796daf3b32c7580bc0421d238ede9f16ab9
tubi55 <tubi55@nate.com>
2023-11-27 13:00:59 +0900

    json객체의 property key도 state에 담아서 제목도 자동화 처리

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -3,17 +3,20 @@ import Layout from '../../common/layout/Layout';
 import './Department.scss';
 
 export default function Department() {
-	console.log('render');
+	const [MemberTit, setMemberTit] = useState('');
+
 	const [MemberData, setMemberData] = useState([]);
-	console.log(MemberData);
 	const path = process.env.PUBLIC_URL; //public폴더까지의 경로를 구하는 구문
 
 	const fetchDepartment = () => {
 		fetch(`${path}/DB/department.json`)
 			.then((data) => data.json())
 			.then((json) => {
-				//console.log(json.members);
-				setMemberData(json.members);
+				//console.log(json);
+				//console.log('key', Object.keys(json)[0]); //객체를 반복돌며 key값만 배열로 반환
+				//console.log('value', Object.values(json)[0]); //객체를 반복돌며 value값만 배열로 반환
+				setMemberTit(Object.keys(json)[0]);
+				setMemberData(Object.values(json)[0]);
 			});
 	};
 
@@ -23,17 +26,20 @@ export default function Department() {
 
 	return (
 		<Layout title={'Deparment'}>
-			{MemberData.map((member, idx) => {
-				return (
-					<article key={member + idx}>
-						<div className='pic'>
-							<img src={`${path}/img/${member.pic}`} alt={member.name} />
-						</div>
-						<h2>{member.name}</h2>
-						<p>{member.position}</p>
-					</article>
-				);
-			})}
+			<section className='memberBox'>
+				<h2>{MemberTit}</h2>
+				{MemberData.map((member, idx) => {
+					return (
+						<article key={member + idx}>
+							<div className='pic'>
+								<img src={`${path}/img/${member.pic}`} alt={member.name} />
+							</div>
+							<h2>{member.name}</h2>
+							<p>{member.position}</p>
+						</article>
+					);
+				})}
+			</section>
 		</Layout>
 	);
 }
@@ -44,4 +50,10 @@ export default function Department() {
 	2. fetch문을 이용해서 특정 URL의 데이터를 가져온뒤 동기적으로 배열로 뽑은 뒤 state에 담아주는 함수 정의
 	3. 위에서 만든 함수를 의존성 배열이 비어있는 useEffect문 안쪽에서 호출 
 	4. State에 담겨있는 Data 배열값을 map으로 반복돌면서 JSX구문 생성
+
+	객체의 property에서 key, value값 반복도는 방법
+	const student = {name: 'David', age:20}
+	//key반복 돌면서 배열반환
+	Object.keys(student); ['name','age'];
+	Object.values(student); ['David',20];
 */
