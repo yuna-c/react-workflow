68b8b369c98c1abd46719e6b72f271637287f4a0
tubi55 <tubi55@nate.com>
2023-11-30 12:16:52 +0900

    params를 통한 상세페이지에 특정 정보값 url로 전달

---
+++ src/components/sub/youtube/Detail.jsx
@@ -0,0 +1,14 @@
+import Layout from '../../common/layout/Layout';
+import './Detail.scss';
+import { useParams } from 'react-router-dom';
+
+export default function Detail() {
+	const { id } = useParams();
+	console.log(id);
+
+	return (
+		<Layout title={'Detail'}>
+			<h3>{id}</h3>
+		</Layout>
+	);
+}
