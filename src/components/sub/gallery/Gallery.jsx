1abdd970cb3e46ca8ef3ba6be708250933a2a2ff
tubi55 <tubi55@nate.com>
2023-12-01 11:48:55 +0900

    갤러리 기본 스타일 및 반응형 처리

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -7,15 +7,14 @@ export default function Gallery() {
 
 	const fetchFlickr = async () => {
 		console.log('flickr');
-		const num = 500;
-		const flickr_api = 'bdb0f9d2ff8d99d4f48419686e1ed53e';
+		const num = 20;
+		const flickr_api = process.env.REACT_APP_FLICKR_API;
 		const method_interest = 'flickr.interestingness.getList';
 		const baseURL = 'https://www.flickr.com/services/rest/?method=';
 		const resultURL = `${baseURL}${method_interest}&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1`;
 
 		const data = await fetch(resultURL);
 		const json = await data.json();
-		console.log(json.photos.photo);
 		setPics(json.photos.photo);
 	};
 
@@ -25,17 +24,21 @@ export default function Gallery() {
 
 	return (
 		<Layout title={'Gallery'}>
-			{Pics.map((pic, idx) => {
-				return (
-					<article key={pic.id}>
-						<h2>{pic.title}</h2>
-						<img
-							src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
-							alt={pic.title}
-						/>
-					</article>
-				);
-			})}
+			<section className='frame'>
+				{Pics.map((pic, idx) => {
+					return (
+						<article key={pic.id}>
+							<div className='pic'>
+								<img
+									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+									alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+								/>
+							</div>
+							<h2>{pic.title}</h2>
+						</article>
+					);
+				})}
+			</section>
 		</Layout>
 	);
 }
