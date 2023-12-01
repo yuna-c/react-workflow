d0cd5a56bf2e1776f9d236fac4adbb8d229a7fed
tubi55 <tubi55@nate.com>
2023-12-01 11:08:35 +0900

    flickr data 호출

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 
 export default function Gallery() {
-	return <Layout title={'Gallery'}>Gallery</Layout>;
+	const [Pics, setPics] = useState([]);
+
+	const fetchFlickr = async () => {
+		console.log('flickr');
+		const num = 500;
+		const flickr_api = 'bdb0f9d2ff8d99d4f48419686e1ed53e';
+		const method_interest = 'flickr.interestingness.getList';
+		const baseURL = 'https://www.flickr.com/services/rest/?method=';
+		const resultURL = `${baseURL}${method_interest}&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1`;
+
+		const data = await fetch(resultURL);
+		const json = await data.json();
+		console.log(json.photos.photo);
+		setPics(json.photos.photo);
+	};
+
+	useEffect(() => {
+		fetchFlickr();
+	}, []);
+
+	return (
+		<Layout title={'Gallery'}>
+			{Pics.map((pic, idx) => {
+				return (
+					<article key={pic.id}>
+						<h2>{pic.title}</h2>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
