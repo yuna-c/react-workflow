2274e58daec64d46636645764312255ae60ed137
tubi55 <tubi55@nate.com>
2023-12-04 12:42:40 +0900

    keyword를 통한 검색 갤러리 로직 추가

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -4,9 +4,7 @@ import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 
 export default function Gallery() {
-	console.log('re-render');
 	const myID = useRef('197119297@N02');
-	//isUser의 초기값을 내 아이디 문자값으로 등록
 	const isUser = useRef(myID.current);
 	const refNav = useRef(null);
 	const [Pics, setPics] = useState([]);
@@ -16,33 +14,24 @@ export default function Gallery() {
 		btns.forEach((btn) => btn.classList.remove('on'));
 		e && e.target.classList.add('on');
 	};
-
 	const handleInterest = (e) => {
 		if (e.target.classList.contains('on')) return;
-		//Interest함수 호출시 isUser값을 빈문자열로 초기화 (false로 인식되는 값)
 		isUser.current = '';
 		activateBtn(e);
 		fetchFlickr({ type: 'interest' });
 	};
-
 	const handleMine = (e) => {
-		//꼭 찍어서 isUser의 값과 myID값이 동일할때만 함수 중지
-		//마이갤러리 함수 호출시에는 isUser의 문자값이 담겨있다고 하더라도 내아이디와 똑같지 않으면 핸들러함수를 실행하게 처리
-		//다른 사용자 갤러리를 갔다가 다시 myGallery호출시 이미 다른 사용자 유저id가 담겨있기 때문에 내 갤러리가 호출되지 않는 문제를 해결하기 위함
 		if (e.target.classList.contains('on') || isUser.current === myID.current) return;
 		isUser.current = myID.current;
 		activateBtn(e);
 		fetchFlickr({ type: 'user', id: myID.current });
 	};
-
 	const handleUser = (e) => {
-		//isUser값이 비어있기만 하면 중지
 		if (isUser.current) return;
 		isUser.current = e.target.innerText;
 		activateBtn();
 		fetchFlickr({ type: 'user', id: e.target.innerText });
 	};
-
 	const fetchFlickr = async (opt) => {
 		const num = 50;
 
@@ -50,18 +39,22 @@ export default function Gallery() {
 		const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
+		const method_search = 'flickr.photos.search';
 		const interestURL = `${baseURL}${method_interest}`;
 		const userURL = `${baseURL}${method_user}&user_id=${opt.id}`;
+		const searchURL = `${baseURL}${method_search}&tags=${opt.keyword}`;
 		let url = '';
 		opt.type === 'user' && (url = userURL);
 		opt.type === 'interest' && (url = interestURL);
+		opt.type === 'search' && (url = searchURL);
 		const data = await fetch(url);
 		const json = await data.json();
 		setPics(json.photos.photo);
 	};
 
 	useEffect(() => {
-		fetchFlickr({ type: 'user', id: myID.current });
+		//fetchFlickr({ type: 'user', id: myID.current });
+		fetchFlickr({ type: 'search', keyword: 'landscape' });
 	}, []);
 
 	return (
@@ -77,7 +70,7 @@ export default function Gallery() {
 
 			<section>
 				<Masonry className={'frame'} options={{ transitionDuration: '0.5s', gutter: 20 }}>
-					{Pics.map((pic, idx) => {
+					{Pics.map((pic) => {
 						return (
 							<article key={pic.id}>
 								<div className='pic'>
