4e272b8f016537fe22f9cf6cfb4581289c98ce29
tubi55 <tubi55@nate.com>
2023-12-05 09:44:08 +0900

    검색폼 검색버튼 추가

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import Masonry from 'react-masonry-component';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
+import { LuSearch } from 'react-icons/lu';
 
 export default function Gallery() {
 	const myID = useRef('197119297@N02');
@@ -34,15 +35,14 @@ export default function Gallery() {
 	};
 	const fetchFlickr = async (opt) => {
 		const num = 50;
-
 		const flickr_api = process.env.REACT_APP_FLICKR_API;
 		const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
-		const method_search = 'flickr.photos.search';
+		const method_search = 'flickr.photos.search'; //search method 추가
 		const interestURL = `${baseURL}${method_interest}`;
 		const userURL = `${baseURL}${method_user}&user_id=${opt.id}`;
-		const searchURL = `${baseURL}${method_search}&tags=${opt.keyword}`;
+		const searchURL = `${baseURL}${method_search}&tags=${opt.keyword}`; //search url 추가
 		let url = '';
 		opt.type === 'user' && (url = userURL);
 		opt.type === 'interest' && (url = interestURL);
@@ -54,7 +54,7 @@ export default function Gallery() {
 
 	useEffect(() => {
 		//fetchFlickr({ type: 'user', id: myID.current });
-		fetchFlickr({ type: 'search', keyword: 'landscape' });
+		fetchFlickr({ type: 'search', keyword: 'ocean' }); //landscape 키워드로 검색 타입 갤리러 호출
 	}, []);
 
 	return (
@@ -66,6 +66,11 @@ export default function Gallery() {
 						My Gallery
 					</button>
 				</nav>
+
+				<form>
+					<input type='text' placeholder='Search' />
+					<LuSearch className='btnSearch' />
+				</form>
 			</article>
 
 			<section>
