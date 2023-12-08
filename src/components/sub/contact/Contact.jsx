197018e50d919e856029e70df1a82c2c5032102d
tubi55 <tubi55@nate.com>
2023-12-08 12:58:36 +0900

    마커 이미지 변경

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -7,17 +7,21 @@ export default function Contact() {
 	const mapFrame = useRef(null);
 
 	const mapOption = useRef({
-		//위치값 정밀하게 보정하는 법
-		//기존 구글지도 위치값 복사한 뒤, 카카오예제의 클릭한위치의 마커표시 직접해보기에서
-		//해당 코드를 붙여넣기 하고 원하는 지점을 찍으면 아래와 같이 정밀한 수치값을 확인 가능
 		center: new kakao.maps.LatLng(37.5127347299894, 127.0607705454063),
 		level: 3,
 	});
 
+	const imgSrc = process.env.PUBLIC_URL + '/img/marker1.png';
+	const imgSize = new kakao.maps.Size(232, 99);
+	const imgOpt = { offset: new kakao.maps.Point(116, 99) };
+
 	useEffect(() => {
 		const mapInstance = new kakao.maps.Map(mapFrame.current, mapOption.current);
+		const markerImageInstance = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOpt);
+
 		const markerInstance = new kakao.maps.Marker({
 			position: mapOption.current.center,
+			image: markerImageInstance,
 		});
 
 		markerInstance.setMap(mapInstance);
