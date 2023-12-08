1e38eac7ac55196a9e98c933a891e4daa2b9a317
tubi55 <tubi55@nate.com>
2023-12-08 12:11:48 +0900

    지도 출력 오류문제 해결 (기존 reset에 *에 height: auto지정으로 kakao api가 지도가 들어갈 높이값 인지 못하던 이유

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -103,7 +103,7 @@ export default function Community() {
 
 	return (
 		<Layout title={'Community'}>
-			<div className='wrap'>
+			<div className='communityWrap'>
 				<div className='inputBox'>
 					<input type='text' placeholder='title' ref={refTit} />
 					<textarea cols='30' rows='3' placeholder='content' ref={refCon}></textarea>
--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useRef } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 
 export default function Contact() {
-	return <Layout title={'Contact'}>Contact</Layout>;
+	const mapFrame = useRef(null);
+	const { kakao } = window;
+	console.log(kakao);
+
+	const mapOption = useRef({
+		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+	});
+
+	useEffect(() => {
+		const map = new kakao.maps.Map(mapFrame.current, mapOption.current);
+	}, [kakao]);
+
+	return (
+		<Layout title={'Contact'}>
+			<article className='mapBox' ref={mapFrame}></article>
+		</Layout>
+	);
 }
