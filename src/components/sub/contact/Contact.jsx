e54702f9f65572f416250d1a95b60e096a3005d8
tubi55 <tubi55@nate.com>
2023-12-08 12:43:13 +0900

    마커 위치 정밀하게 보정하기

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -7,7 +7,10 @@ export default function Contact() {
 	const mapFrame = useRef(null);
 
 	const mapOption = useRef({
-		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		//위치값 정밀하게 보정하는 법
+		//기존 구글지도 위치값 복사한 뒤, 카카오예제의 클릭한위치의 마커표시 직접해보기에서
+		//해당 코드를 붙여넣기 하고 원하는 지점을 찍으면 아래와 같이 정밀한 수치값을 확인 가능
+		center: new kakao.maps.LatLng(37.5127347299894, 127.0607705454063),
 		level: 3,
 	});
 
