e2ab6668f1000219bd2c9a93958abe46f3764cbf
tubi55 <tubi55@nate.com>
2023-12-11 10:07:24 +0900

    kakao객체를 참조객체에 옮겨담기

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -3,7 +3,7 @@ import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 
 export default function Contact() {
-	const { kakao } = window;
+	const kakao = useRef(window.kakao);
 
 	//화면에 출력될 지도정보 배열의 순번이 담길 state
 	const [Index, setIndex] = useState(0);
@@ -14,10 +14,10 @@ export default function Contact() {
 	const mapInfo = useRef([
 		{
 			title: '삼성역 코엑스',
-			latlng: new kakao.maps.LatLng(37.51100661425726, 127.06162026853143),
+			latlng: new kakao.current.maps.LatLng(37.51100661425726, 127.06162026853143),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) },
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
 		},
 		{
 			title: '넥슨 본사',
@@ -28,15 +28,15 @@ export default function Contact() {
 		},
 		{
 			title: '서울 시청',
-			latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
+			latlng: new kakao.current.maps.LatLng(37.5662952, 126.9779451),
 			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
-			imgSize: new kakao.maps.Size(232, 99),
-			imgPos: { offset: new kakao.maps.Point(116, 99) },
+			imgSize: new kakao.current.maps.Size(232, 99),
+			imgPos: { offset: new kakao.current.maps.Point(116, 99) },
 		},
 	]);
 
 	//마커 인스턴스 생성
-	marker.current = new kakao.maps.Marker({
+	marker.current = new kakao.current.maps.Marker({
 		position: mapInfo.current[Index].latlng,
 		image: new kakao.maps.MarkerImage(
 			mapInfo.current[Index].imgSrc,
@@ -47,12 +47,12 @@ export default function Contact() {
 
 	//컴포넌트 마운트시 참조객체에 담아놓은 돔 프레임에 지도 인스턴스 출력 및 마커 세팅
 	useEffect(() => {
-		const mapInstance = new kakao.maps.Map(mapFrame.current, {
+		const mapInstance = new kakao.current.maps.Map(mapFrame.current, {
 			center: mapInfo.current[Index].latlng,
 			level: 3,
 		});
 		marker.current.setMap(mapInstance);
-	}, [Index, kakao]);
+	}, [Index]);
 
 	return (
 		<Layout title={'Contact'}>
