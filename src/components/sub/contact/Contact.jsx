46b8eb08c87ecdd22775c1f1233882b4014eb247
tubi55 <tubi55@nate.com>
2023-12-12 11:09:07 +0900

    폼메일 연결

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,8 +1,36 @@
 import { useEffect, useRef, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+	const form = useRef();
+
+	const resetForm = () =>{
+		const elArr = form.current.children;
+
+		Array.from(elArr).forEach((el)=> {
+			console.log(el)
+			if( el.name==='user_name' || el.name==='user_email'||el.name==='message' ) el.value='';
+		})
+	}
+
+	const sendEmail = e => {
+		e.preventDefault();	
+
+
+		emailjs.sendForm('service_ag7z96s', 'template_oh9ajns', form.current, '23g8RepczesqKPoIX').then(
+			result => {
+				alert('문의 내용이 성공적으로 전송되었습니다.');
+				resetForm();
+			},
+			error => {
+				alert('일시적인 장애로 문의 전송에 실패했습니다. 다음의 메일주소로 보내주세요.')
+				resetForm();
+			}
+		);
+	};
+
 	const kakao = useRef(window.kakao);
 
 	//화면에 출력될 지도정보 배열의 순번이 담길 state
@@ -91,24 +119,38 @@ export default function Contact() {
 
 	return (
 		<Layout title={'Contact'}>
-			<div className='controlBox'>
-				<nav className='branch'>
-					{mapInfo.current.map((el, idx) =>
-						//prettier-ignore
-						<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>{el.title}</button>
-					)}
-				</nav>
-
-				<nav className='info'>
-					<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic OFF' : 'Traffic ON'}</button>
-					<button onClick={() => setView(!View)}>{View ? 'map' : 'road view'}</button>
-					<button onClick={setCenter}>위치 초기화</button>
-				</nav>
+			<div id='mailSection'>
+				<form ref={form} onSubmit={sendEmail}>
+					<label>Name</label>
+					<input type='text' name='user_name' />
+					<label>Email</label>
+					<input type='email' name='user_email' />
+					<label>Message</label>
+					<textarea name='message' />
+					<input type='submit' value='Send' />
+				</form>
 			</div>
-			<section className='tab'>
-				<article className={`mapBox ${View ? '' : 'on'}`} ref={mapFrame}></article>
-				<article className={`viewBox ${View ? 'on' : ''}`} ref={viewFrame}></article>
-			</section>
+
+			<div id='mapSection'>
+				<div className='controlBox'>
+					<nav className='branch'>
+						{mapInfo.current.map((el, idx) =>
+							//prettier-ignore
+							<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>{el.title}</button>
+						)}
+					</nav>
+
+					<nav className='info'>
+						<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic OFF' : 'Traffic ON'}</button>
+						<button onClick={() => setView(!View)}>{View ? 'map' : 'road view'}</button>
+						<button onClick={setCenter}>위치 초기화</button>
+					</nav>
+				</div>
+				<section className='tab'>
+					<article className={`mapBox ${View ? '' : 'on'}`} ref={mapFrame}></article>
+					<article className={`viewBox ${View ? 'on' : ''}`} ref={viewFrame}></article>
+				</section>
+			</div>
 		</Layout>
 	);
 }
