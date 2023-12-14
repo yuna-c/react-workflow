73d8134fffdf742fdc306001c21df86f64c0a262
tubi55 <tubi55@nate.com>
2023-12-14 12:33:51 +0900

    fotter에서 전역 데이터 호출

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,13 +1,19 @@
 import './Footer.scss';
 import { FaFacebookF, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 //npm i react-icons
 
 export default function Footer() {
+	const { name, position } = useSelector(store => store.memberReducer.members[0]);
+
 	return (
 		<footer className='Footer'>
 			<h1>Dcodelab</h1>
 
 			<p>2023 Dcodelab &copy; All Rights Reserved.</p>
+			<p>
+				{position}:{name}
+			</p>
 
 			<ul>
 				<li>
