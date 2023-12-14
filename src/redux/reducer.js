8a348bfe9efd61d45f291e2f648de162ca1c32a8
tubi55 <tubi55@nate.com>
2023-12-14 11:52:10 +0900

    리듀서에 전달받은 데이터를 store 전역객체에 저장하고 App루트 컴포넌트로 전달

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -37,16 +37,6 @@ const initMember = {
 
 //초기 데이터값을 state로 지정하고 추후 action객체가 넘어오면 action의 타입에 따라서 해당 데이터를 변경해주는 변형자함수
 //{type:'SET_MEMBERS': payload:[변경할데이터배열] }
-//{type:'ANYTHING': payload: err}
-/*
-const memberReducer = (state = initMember, action) => {
-	if (action.type === 'SET_MEMBERS') {
-		return { ...state, members: action.payload };
-	} else {
-		return state;
-	}
-};
-*/
 const memberReducer = (state = initMember, action) => {
 	switch (action.type) {
 		case 'SET_MEMBERS':
@@ -59,3 +49,13 @@ const memberReducer = (state = initMember, action) => {
 //해당 파일에서 내보내는 여러개의 리듀서객체를 합쳐서 외부로 export
 const reducers = combineReducers({ memberReducer });
 export default reducers;
+
+/*
+const memberReducer = (state = initMember, action) => {
+	if (action.type === 'SET_MEMBERS') {
+		return { ...state, members: action.payload };
+	} else {
+		return state;
+	}
+};
+*/
