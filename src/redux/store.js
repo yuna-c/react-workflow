868d6accd569b97948eecba0bbe8cb447c475a5e
tubi55 <tubi55@nate.com>
2023-12-18 12:48:41 +0900

    전역객체 루트컴포넌트인 App에 전달

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import createSagaMiddleware from '@redux-saga/core';
 import rootSaga from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
-
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+
 sagaMiddleware.run(rootSaga);
 export default store;
