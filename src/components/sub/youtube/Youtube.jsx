53c35446a97c58ae460f1f9a7c3b98d57d04a86f
tubi55 <tubi55@nate.com>
2023-12-21 13:27:21 +0900

    컴포넌트 언마운트시 동작되면 안되는 코드 분기처리

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -3,37 +3,45 @@ import './Youtube.scss';
 import { useCustomText } from '../../../hooks/useText';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Youtube() {
+	const [Mounted, setMounted] = useState(true);
+
 	const Vids = useSelector(store => store.youtubeReducer.youtube);
 	const customText = useCustomText('combined');
 	const shortenText = useCustomText('shorten');
 
+	useEffect(() => {
+		return () => setMounted(false);
+	}, [Mounted]);
+
 	return (
 		<Layout title={'Youtube'}>
-			{Vids?.map(data => {
-				const [date, time] = data.snippet.publishedAt.split('T');
+			{Mounted &&
+				Vids?.map(data => {
+					const [date, time] = data.snippet.publishedAt.split('T');
 
-				return (
-					<article key={data.id}>
-						<h2>{shortenText(data.snippet.title, 50)}</h2>
+					return (
+						<article key={data.id}>
+							<h2>{shortenText(data.snippet.title, 50)}</h2>
 
-						<div className='txt'>
-							<p>{shortenText(data.snippet.description, 250)}</p>
-							<div className='infoBox'>
-								<span>{customText(date, '.')}</span>
-								<em>{time.split('Z')[0]}</em>
+							<div className='txt'>
+								<p>{shortenText(data.snippet.description, 250)}</p>
+								<div className='infoBox'>
+									<span>{customText(date, '.')}</span>
+									<em>{time.split('Z')[0]}</em>
+								</div>
 							</div>
-						</div>
 
-						<div className='pic'>
-							<Link to={`/detail/${data.id}`}>
-								<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
-							</Link>
-						</div>
-					</article>
-				);
-			})}
+							<div className='pic'>
+								<Link to={`/detail/${data.id}`}>
+									<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+								</Link>
+							</div>
+						</article>
+					);
+				})}
 		</Layout>
 	);
 }
