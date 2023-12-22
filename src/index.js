f780e5081ff530d1c7c2dd069c8b29b371a181b6
tubi55 <tubi55@nate.com>
2023-12-22 09:48:25 +0900

    redux버전에서의 중요한 이슈사항 정리

--- src/index.js
+++ src/index.js
@@ -13,3 +13,11 @@ ReactDOM.render(
 	</BrowserRouter>,
 	document.getElementById('root')
 );
+
+/*
+	Redux버전에서 숙지할 내용
+	1.flickr빼고 비동기 데이터 사용 컴포넌트에서 store로 데이터 공유 (Member, History, Youtube)
+	2.client side data가 store로 공유 (Modal, Memu, Dark)
+	3.Layout에서 0.3초있다 on붙게 되는데 라우터이동이 0.3초보다 빨리 이동될때 optionial chaing으로 에러핸들링
+	4.Contact컴포넌트에서 throttle이 적용된 throttledSetCenter resize이벤트 연결문을 따로 useEffect로 분리
+*/
