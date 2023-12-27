7d69574ed29f1bb506db8381801d948e3944bc79
tubi55 <tubi55@nate.com>
2023-12-27 17:52:00 +0900

    queryKey값 파라미터단에서 다시 비구조화할당

--- src/hooks/useFlickrQuery.js
+++ src/hooks/useFlickrQuery.js
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 
-const fetchFlickr = async ({ queryKey }) => {
+const fetchFlickr = async ({ queryKey: [_, opt] }) => {
 	const num = 50;
 	const flickr_api = process.env.REACT_APP_FLICKR_API;
 	const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
@@ -8,20 +8,18 @@ const fetchFlickr = async ({ queryKey }) => {
 	const method_user = 'flickr.people.getPhotos';
 	const method_search = 'flickr.photos.search'; //search method 추가
 	const interestURL = `${baseURL}${method_interest}`;
-	const userURL = `${baseURL}${method_user}&user_id=${queryKey[1].id}`;
-	const searchURL = `${baseURL}${method_search}&tags=${queryKey[1].keyword}`; //search url 추가
+	const userURL = `${baseURL}${method_user}&user_id=${opt.id}`;
+	const searchURL = `${baseURL}${method_search}&tags=${opt.keyword}`; //search url 추가
 	let url = '';
-	queryKey[1].type === 'user' && (url = userURL);
-	queryKey[1].type === 'interest' && (url = interestURL);
-	queryKey[1].type === 'search' && (url = searchURL);
+	opt.type === 'user' && (url = userURL);
+	opt.type === 'interest' && (url = interestURL);
+	opt.type === 'search' && (url = searchURL);
 	const data = await fetch(url);
 	const json = await data.json();
-	console.log(json);
 	return json.photos.photo;
 };
 
 export const useFlickrQuery = opt => {
-	console.log(opt);
 	return useQuery(['fetchFlickr', opt], fetchFlickr, {
 		refetchOnMount: false,
 		refetchOnWindowFocus: false,
