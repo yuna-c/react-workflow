94daba8b2989e00c302f6ff6c6097ed0741a1902
tubi55 <tubi55@nate.com>
2023-12-27 13:16:48 +0900

    유튜브 데이터 리액트 쿼리로 관리

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,61 +1,40 @@
 import Layout from '../../common/layout/Layout';
 import './Youtube.scss';
-import { useState, useEffect } from 'react';
 import { useCustomText } from '../../../hooks/useText';
 import { Link } from 'react-router-dom';
+import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 
 export default function Youtube() {
 	const customText = useCustomText('combined');
 	const shortenText = useCustomText('shorten');
-	const [Vids, setVids] = useState([]);
-
-	const fetchYoutube = async () => {
-		const api_key = process.env.REACT_APP_YOUTUBE_API;
-		const pid = process.env.REACT_APP_YOUTUBE_LIST;
-		const num = 10;
-		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
-
-		try {
-			const data = await fetch(baseURL);
-			const json = await data.json();
-			setVids(json.items);
-		} catch (err) {
-			console.error(err);
-		}
-	};
-
-	useEffect(() => {
-		fetchYoutube();
-	}, []);
+	const { data: Vids, isSuccess } = useYoutubeQuery();
 
 	return (
 		<Layout title={'Youtube'}>
-			{Vids.map((data) => {
-				const [date, time] = data.snippet.publishedAt.split('T');
+			{isSuccess &&
+				Vids.map(data => {
+					const [date, time] = data.snippet.publishedAt.split('T');
 
-				return (
-					<article key={data.id}>
-						<h2>{shortenText(data.snippet.title, 50)}</h2>
+					return (
+						<article key={data.id}>
+							<h2>{shortenText(data.snippet.title, 50)}</h2>
 
-						<div className='txt'>
-							<p>{shortenText(data.snippet.description, 250)}</p>
-							<div className='infoBox'>
-								<span>{customText(date, '.')}</span>
-								<em>{time.split('Z')[0]}</em>
+							<div className='txt'>
+								<p>{shortenText(data.snippet.description, 250)}</p>
+								<div className='infoBox'>
+									<span>{customText(date, '.')}</span>
+									<em>{time.split('Z')[0]}</em>
+								</div>
 							</div>
-						</div>
 
-						<div className='pic'>
-							<Link to={`/detail/${data.id}`}>
-								<img
-									src={data.snippet.thumbnails.standard.url}
-									alt={data.snippet.title}
-								/>
-							</Link>
-						</div>
-					</article>
-				);
-			})}
+							<div className='pic'>
+								<Link to={`/detail/${data.id}`}>
+									<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+								</Link>
+							</div>
+						</article>
+					);
+				})}
 		</Layout>
 	);
 }
