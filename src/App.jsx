02a72ca8b0fd9874dd01ab0f76a8457b0708d7bb
tubi55 <tubi55@nate.com>
2023-12-27 12:42:09 +0900

    react-qeury 세팅완료

--- src/App.jsx
+++ src/App.jsx
@@ -16,25 +16,32 @@ import Menu from './components/common/memu/Menu';
 import Detail from './components/sub/youtube/Detail';
 import Welcome from './components/sub/members/Welcome';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 //git confige option 수정
 export default function App() {
 	const [Dark, setDark] = useState(false);
 	const [Toggle, setToggle] = useState(false);
+	const queryClient = new QueryClient();
 
 	return (
-		<div className={`wrap ${Dark ? 'dark' : ''} ${useMedia()}`}>
-			<Header Dark={Dark} setDark={setDark} Toggle={Toggle} setToggle={setToggle} />
-			<Route exact path='/' component={MainWrap} />
-			<Route path='/department' component={Department} />
-			<Route path='/gallery' component={Gallery} />
-			<Route path='/community' component={Community} />
-			<Route path='/members' component={Members} />
-			<Route path='/contact' component={Contact} />
-			<Route path='/youtube' component={Youtube} />
-			<Route path='/detail/:id' component={Detail} />
-			<Route path='/welcome/:id' component={Welcome} />
-			<Footer />
-			{Toggle && <Menu setToggle={setToggle} />}
-		</div>
+		<QueryClientProvider client={queryClient}>
+			<div className={`wrap ${Dark ? 'dark' : ''} ${useMedia()}`}>
+				<Header Dark={Dark} setDark={setDark} Toggle={Toggle} setToggle={setToggle} />
+				<Route exact path='/' component={MainWrap} />
+				<Route path='/department' component={Department} />
+				<Route path='/gallery' component={Gallery} />
+				<Route path='/community' component={Community} />
+				<Route path='/members' component={Members} />
+				<Route path='/contact' component={Contact} />
+				<Route path='/youtube' component={Youtube} />
+				<Route path='/detail/:id' component={Detail} />
+				<Route path='/welcome/:id' component={Welcome} />
+				<Footer />
+				{Toggle && <Menu setToggle={setToggle} />}
+			</div>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }
