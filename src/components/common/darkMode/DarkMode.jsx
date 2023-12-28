87f67d7194333f3fa204e1164f1ee04db22ab83f
tubi55 <tubi55@nate.com>
2023-12-28 15:49:04 +0900

    쿠키에 Dark state정보값 저장

--- src/components/common/darkMode/DarkMode.jsx
+++ src/components/common/darkMode/DarkMode.jsx
@@ -1,8 +1,19 @@
+import { useCookie } from '../../../hooks/useCookie';
+import { useGlobalData } from '../../../hooks/useGlobalData';
 import './DarkMode.scss';
 
 export default function DarkMode() {
+	const { setCookie } = useCookie();
+	const { Dark, setDark } = useGlobalData();
+
+	const changeMode = () => {
+		setDark(!Dark);
+		//토글 이벤트함수가 호출될때 반전되는 Dark state값을 쿠키에 저장
+		setCookie('dark', !Dark, 20);
+	};
+
 	return (
-		<div className={`DarkMode`}>
+		<div className={`DarkMode ${Dark ? 'dark' : ''}`} onClick={changeMode}>
 			<div className='ball'></div>
 		</div>
 	);
