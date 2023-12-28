2c0d53122832311bf832b4bb299b90cca8e279ee
tubi55 <tubi55@nate.com>
2023-12-28 13:11:46 +0900

    이벤트 발생시 body에 등록된 css변수값 새로 세팅

--- src/components/common/themeControl/ThemeControl.jsx
+++ src/components/common/themeControl/ThemeControl.jsx
@@ -1,10 +1,24 @@
 import './ThemeControl.scss';
+import { useCookie } from '../../../hooks/useCookie';
+import { useRef } from 'react';
 
 export default function ThemeControl() {
+	const inputEl = useRef(null);
+
+	const { setCookie } = useCookie();
+
+	const changeThemeColor = () => {
+		const color = inputEl.current.value;
+		console.log(color);
+		setCookie('theme', color, 20);
+		console.log(getComputedStyle(document.body).getPropertyValue('--pointColor'));
+		document.body.style.setProperty('--pointColor', color);
+	};
+
 	return (
 		<nav className='ThemeControl'>
-			<input type='color' />
-			<button>theme color</button>
+			<input type='color' ref={inputEl} />
+			<button onClick={changeThemeColor}>theme color</button>
 		</nav>
 	);
 }
