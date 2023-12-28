c9070e6e480b43e7a54ea8c3ce2009b4ee49fdc4
tubi55 <tubi55@nate.com>
2023-12-28 11:15:44 +0900

    커스텀훅에 종류별로 쿠키제어 관련 함수 추가

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -4,11 +4,8 @@ import { FaFacebookF, FaTwitter, FaYoutube } from 'react-icons/fa';
 //npm i react-icons
 
 export default function Footer() {
-	const setCookie = useCookie();
-	const createCookie = () => {
-		setCookie('today', 'done', 20);
-	};
-	console.log(document.cookie);
+	const { setCookie, isCookie, viewCookie } = useCookie();
+
 	return (
 		<footer className='Footer'>
 			<h1>Dcodelab</h1>
@@ -27,7 +24,10 @@ export default function Footer() {
 				</li>
 			</ul>
 
-			<button onClick={createCookie}>쿠키생성</button>
+			<button onClick={() => setCookie('today', 'done', 60 * 60)}>쿠키생성</button>
+			<button onClick={() => setCookie('today', 'done', 0)}>쿠키삭제</button>
+			<button onClick={() => console.log(isCookie('today=done'))}>쿠키확인</button>
+			<button onClick={() => viewCookie()}>모든 쿠키 보기</button>
 		</footer>
 	);
 }
