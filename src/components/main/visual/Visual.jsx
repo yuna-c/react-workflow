fafaf769d2171eb9d3ea035eda524690d0950e18
tubi55 <tubi55@nate.com>
2023-12-29 11:50:41 +0900

    react에 자동롤링 기능 정지, 추가 커스텀 이벤트 연결

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,9 +1,21 @@
 import './Visual.scss';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper';
 import { useSelector } from 'react-redux';
+
+function Btns() {
+	//Swiper컴포넌트 안쪽에 있는 또다른 자식 컴포넌트 안쪽에서만 useSwiper hook사용가능
+	//hook으로부터 생성된 객체(인스턴스)에는 다양한 prototype메서드와 property값 활용가능
+	const swiper = useSwiper();
+	return (
+		<nav className='swiperController'>
+			<button onClick={() => swiper.autoplay.start()}>start</button>
+			<button onClick={() => swiper.autoplay.stop()}>stop</button>
+		</nav>
+	);
+}
 
 export default function Visual() {
 	const { youtube } = useSelector(store => store.youtubeReducer);
@@ -11,12 +23,16 @@ export default function Visual() {
 	return (
 		<figure className='Visual'>
 			<Swiper
-				modules={[Pagination]}
+				modules={[Pagination, Autoplay]}
 				pagination={{
 					clickable: true,
 					renderBullet: (index, className) => {
 						return `<span class=${className}>${index + 1}</span>`;
 					}
+				}}
+				autoplay={{
+					delay: 2000,
+					disableOnInteraction: true
 				}}
 				loop={true}>
 				{youtube.map((vid, idx) => {
@@ -29,7 +45,15 @@ export default function Visual() {
 						</SwiperSlide>
 					);
 				})}
+
+				<Btns />
 			</Swiper>
 		</figure>
 	);
 }
+
+/*
+	React에서 Swiper의 코어기능을 적용하기 위해서는 useSwiper라는 hook호출
+	Swiper안쪽에서 또다른 컴포넌트를 연결해주고 그 안쪽에서 useSwiper로 부터 객체생성
+	해당 자식 자식 컴포넌트 안쪽에서 생성된 객체로부터 swiper core에 등록되어 있는 모든 메서드, 프로퍼티를 리액트에서도 사용가능
+*/
