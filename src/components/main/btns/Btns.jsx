fd6b8a91a2e70f768d13c804415022caa8c4c1a1
tubi55 <tubi55@nate.com>
2024-01-04 15:49:26 +0900

    모션중 재이벤트 방지

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -9,6 +9,7 @@ export default function Btns() {
 	const secs = useRef(null);
 	const btns = useRef(null);
 	const baseLine = useRef(-window.innerHeight / 2);
+	const isMotion = useRef(false);
 
 	const activation = () => {
 		const scroll = wrap.current.scrollTop;
@@ -22,25 +23,20 @@ export default function Btns() {
 	};
 
 	const moveScroll = idx => {
+		if (isMotion.current) return;
+		isMotion.current = true;
 		console.log('move');
-		new Anime(wrap.current, { scroll: secs.current[idx].offsetTop }, { duration: 500 });
+		new Anime(wrap.current, { scroll: secs.current[idx].offsetTop }, { callback: () => (isMotion.current = false) });
 	};
 
 	const autoScroll = e => {
 		const btnsArr = Array.from(btns.current.children);
 		const activeEl = btns.current.querySelector('li.on');
-		//현재 활성화된 버튼의 순번구함
 		const activeIndex = btnsArr.indexOf(activeEl);
 
-		//휠 다운시
 		if (e.deltaY > 0) {
-			console.log('wheel down');
-			//현재순번이 마지막순번이 아니면 다음순번 섹션위치로 모션이동
 			activeIndex !== Num - 1 && moveScroll(activeIndex + 1);
 		} else {
-			//휠 업시
-			console.log('wheel up');
-			//현재순번이 첫번째순번이 아니면 이전순번 섹션 위치로 모션이동
 			activeIndex !== 0 && moveScroll(activeIndex - 1);
 		}
 	};
@@ -52,8 +48,9 @@ export default function Btns() {
 		secs.current = wrap.current.querySelectorAll('.myScroll');
 		setNum(secs.current.length);
 
-		wrap.current.addEventListener('mousewheel', autoScroll);
 		wrap.current.addEventListener('scroll', throttledActivation);
+		wrap.current.addEventListener('mousewheel', autoScroll);
+
 		return () => {
 			wrap.current.removeEventListener('scroll', throttledActivation);
 			wrap.current.removeEventListener('mousewheel', autoScroll);
