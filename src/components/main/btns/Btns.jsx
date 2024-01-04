495f3363297e9f2ca1ac6bc7c16dc5b023bfeed0
tubi55 <tubi55@nate.com>
2024-01-04 15:09:13 +0900

    autoScroll기능 적용

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -8,7 +8,7 @@ export default function Btns() {
 	const wrap = useRef(null);
 	const secs = useRef(null);
 	const btns = useRef(null);
-	const baseLine = useRef(-window.innerHeight / 2); //현재 섹션의 컨텐츠가 절반이상 보여야지 활성화처리
+	const baseLine = useRef(-window.innerHeight / 2);
 
 	const activation = () => {
 		const scroll = wrap.current.scrollTop;
@@ -22,7 +22,27 @@ export default function Btns() {
 	};
 
 	const moveScroll = idx => {
+		console.log('move');
 		new Anime(wrap.current, { scroll: secs.current[idx].offsetTop }, { duration: 500 });
+	};
+
+	const autoScroll = e => {
+		const btnsArr = Array.from(btns.current.children);
+		const activeEl = btns.current.querySelector('li.on');
+		//현재 활성화된 버튼의 순번구함
+		const activeIndex = btnsArr.indexOf(activeEl);
+
+		//휠 다운시
+		if (e.deltaY > 0) {
+			console.log('wheel down');
+			//현재순번이 마지막순번이 아니면 다음순번 섹션위치로 모션이동
+			activeIndex !== Num - 1 && moveScroll(activeIndex + 1);
+		} else {
+			//휠 업시
+			console.log('wheel up');
+			//현재순번이 첫번째순번이 아니면 이전순번 섹션 위치로 모션이동
+			activeIndex !== 0 && moveScroll(activeIndex - 1);
+		}
 	};
 
 	const throttledActivation = useThrottle(activation);
@@ -32,8 +52,12 @@ export default function Btns() {
 		secs.current = wrap.current.querySelectorAll('.myScroll');
 		setNum(secs.current.length);
 
+		wrap.current.addEventListener('mousewheel', autoScroll);
 		wrap.current.addEventListener('scroll', throttledActivation);
-		return () => wrap.current.removeEventListener('scroll', throttledActivation);
+		return () => {
+			wrap.current.removeEventListener('scroll', throttledActivation);
+			wrap.current.removeEventListener('mousewheel', autoScroll);
+		};
 	}, [throttledActivation]);
 
 	return (
