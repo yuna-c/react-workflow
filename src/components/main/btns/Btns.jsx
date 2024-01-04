b35b95bddbc3f981bf3497fb5633da5a306cd677
tubi55 <tubi55@nate.com>
2024-01-04 11:30:42 +0900

    전체 섹션 갯수값 state에 담기

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,20 +1,19 @@
 import './Btns.scss';
 import { useRef, useState, useEffect } from 'react';
-
 //window.scrollY : 브라우저를 스크롤할때마다 스크롤되고 있는 거리값 (동적)
 //DOM.scrollTop : DOM요소안쪽에서 스크롤할때마다 스크롤되는 거리값 (동적)
 //DOM.offsetTop : 문서에서 해당 돔요소의 세로 위치값 (정적)
 
 export default function Btns() {
 	const [Index, setIndex] = useState(0);
-	const num = useRef(4);
-	const btns = useRef(null);
+	const [Num, setNum] = useState(0);
 	const wrap = useRef(null);
 	const secs = useRef(null);
 
 	useEffect(() => {
 		wrap.current = document.querySelector('.wrap');
 		secs.current = document.querySelectorAll('.myScroll');
+		setNum(secs.current.length);
 
 		wrap.current.addEventListener('scroll', e => {
 			console.log('scroll', e.target.scrollTop);
@@ -24,7 +23,7 @@ export default function Btns() {
 
 	return (
 		<ul className='Btns'>
-			{Array(num.current)
+			{Array(Num)
 				.fill()
 				.map((_, idx) => {
 					return <li key={idx} className={idx === Index ? 'on' : ''}></li>;
