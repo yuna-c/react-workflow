c71d971de167a3dd9afb1465a777c783e46fe0e8
tubi55 <tubi55@nate.com>
2024-01-05 10:07:30 +0900

    스크롤 모션 기능 재활용가능하도록 Btns props설정

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -3,13 +3,15 @@ import './Btns.scss';
 import { useRef, useState, useEffect, useCallback } from 'react';
 import { useThrottle } from '../../../hooks/useThrottle';
 
-export default function Btns() {
+export default function Btns(opt) {
+	const defOpt = useRef({ frame: '.wrap', items: '.myScroll', base: -window.innerHeight / 2, isAuto: false });
+	const resultOpt = useRef({ ...defOpt.current, ...opt });
 	const [Num, setNum] = useState(0);
-	const isAutoScroll = useRef(false);
+	const isAutoScroll = useRef(resultOpt.current.isAuto);
 	const wrap = useRef(null);
 	const secs = useRef(null);
 	const btns = useRef(null);
-	const baseLine = useRef(-window.innerHeight / 2);
+	const baseLine = useRef(resultOpt.current.base);
 	const isMotion = useRef(false);
 
 	const activation = () => {
@@ -24,13 +26,8 @@ export default function Btns() {
 	};
 
 	const moveScroll = idx => {
-		//초기값이 false이므로 처음 한번은 해당 조건문이 무시되면서 아래 코드실행됨
 		if (isMotion.current) return;
-		//조건문을 통과하자마자 바로 값을 true로 변경해서 다음부터는 재호출 안되도록 막음
 		isMotion.current = true;
-		console.log('move');
-		//모션함수가 실행되고 모션이 끝나는순간 실행되는 callback으로 다시 isMotion.current값을 false로 변경해서 재실행 가능케 설정
-		//결론 isMotion.current값을 이용해서 모션중에는 중복 함수호출 불가능하도록 모션중 재이벤트방지 처리
 		new Anime(wrap.current, { scroll: secs.current[idx].offsetTop }, { callback: () => (isMotion.current = false) });
 	};
 
@@ -50,7 +47,6 @@ export default function Btns() {
 	);
 
 	const modifyPos = () => {
-		console.log('modify');
 		const btnsArr = Array.from(btns.current.children);
 		const activeEl = btns.current.querySelector('li.on');
 		const activeIndex = btnsArr.indexOf(activeEl);
@@ -61,8 +57,8 @@ export default function Btns() {
 	const throttledModifyPos = useThrottle(modifyPos, 200);
 
 	useEffect(() => {
-		wrap.current = document.querySelector('.wrap');
-		secs.current = wrap.current.querySelectorAll('.myScroll');
+		wrap.current = document.querySelector(resultOpt.current.frame);
+		secs.current = wrap.current.querySelectorAll(resultOpt.current.items);
 		setNum(secs.current.length);
 
 		window.addEventListener('resize', throttledModifyPos);
@@ -74,7 +70,7 @@ export default function Btns() {
 			wrap.current.removeEventListener('scroll', throttledActivation);
 			wrap.current.removeEventListener('mousewheel', autoScroll);
 		};
-	}, [throttledActivation, autoScroll, throttledModifyPos]);
+	}, [throttledActivation, autoScroll, throttledModifyPos, resultOpt.current.frame, resultOpt.current.items]);
 
 	return (
 		<ul className='Btns' ref={btns}>
