253faccbb7513adab1cea1a70d191eec2148ba50
tubi55 <tubi55@nate.com>
2024-01-05 17:16:34 +0900

    스크롤 커스텀 훅 오류 수정

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -3,14 +3,13 @@ import { useScroll } from '../../../hooks/useScroll';
 import './Pics.scss';
 
 export default function Pics() {
-	console.log('Pics');
 	const [Frame, setFrame] = useState(null);
 	const thisEl = useRef(null);
 	const boxEl = useRef(null);
 	const { getCurrentScroll } = useScroll(Frame);
 
 	const handleScroll = useCallback(() => {
-		const scroll = getCurrentScroll(thisEl.current);
+		const scroll = getCurrentScroll(thisEl.current, -window.innerHeight / 2);
 
 		scroll >= 0 && (boxEl.current.style.transform = `translateX(${scroll}px)`);
 	}, [getCurrentScroll]);
