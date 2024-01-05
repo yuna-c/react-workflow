351eeb97af83016faff4baf944ca33354a0cf6d3
tubi55 <tubi55@nate.com>
2024-01-05 16:00:49 +0900

    Scrolled값 연동

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,19 +1,17 @@
 import Anime from '../asset/anime';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 
 export function useScroll(frame = '.wrap') {
-	//스크롤이벤트가 발생하는 프레임 요소
 	const scrollFrame = useRef(null);
 
-	//특정위치로 스크롤 이동 메서드
 	const scrollTo = targetPos => {
 		new Anime(scrollFrame.current, { scroll: targetPos });
 	};
 
-	//실시간 scroll값 반환 메서드
-	const getCurrentScroll = () => {
+	const getCurrentScroll = selfEl => {
 		const scroll = scrollFrame.current.scrollTop;
-		return scroll;
+		const modifiedScroll = scroll - selfEl?.offsetTop;
+		return modifiedScroll;
 	};
 
 	useEffect(() => {
