274184f2bd2e13ac3b61d970f1e1f17fea89055d
tubi55 <tubi55@nate.com>
2024-01-08 12:01:05 +0900

    scroll연동영역을 벗어나면 원래수치값으로 강제 초기화

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -7,12 +7,16 @@ export default function Pics() {
 	const titEl2 = useRef(null);
 
 	const handleCustomScroll = scroll => {
-		console.log(scroll);
 		if (scroll >= 0) {
 			titEl.current.style.transform = `translateX(${scroll}px)`;
 			titEl.current.style.opacity = 1 - scroll / 800;
 			titEl2.current.style.transform = ` scale(${1 + scroll / 400}) translateX(${scroll}px)`;
 			titEl2.current.style.opacity = 1 - scroll / 500;
+		} else {
+			titEl.current.style.transform = `translateX(0px)`;
+			titEl.current.style.opacity = 1;
+			titEl2.current.style.transform = ` scale(1) translateX(0px)`;
+			titEl2.current.style.opacity = 1;
 		}
 	};
 
--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -10,8 +10,6 @@ export function useScroll(customHandler, baseLine = -window.innerHeight / 2) {
 	};
 
 	const getCurrentScroll = useCallback(() => {
-		//기존 Btns컴포넌트에 baseLine적용시에는 기존 setction의 offsetTop값에 baseLine값을 빼서 기준점을 올리는 방식
-		//해당 훅에서는 반대로 기준점은 그대로 놔두고 동적으로 변하는 scrollTop에 baseLine값을 더해서 기준점을 올리는 방식
 		const scroll = Frame.scrollTop - baseLine;
 		const modifiedScroll = scroll - refEl.current?.offsetTop;
 		return modifiedScroll;
