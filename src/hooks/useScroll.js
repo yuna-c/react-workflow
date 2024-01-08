5e81c7f17821069fd3626734ed4a1916724f52a1
tubi55 <tubi55@nate.com>
2024-01-08 10:21:28 +0900

    useScroll안쪽에서 자체적으로 빈 참조객체 만들어서 부모 컴포넌트에 반환해줌

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,15 +1,18 @@
 import Anime from '../asset/anime';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export function useScroll() {
+	//순서1 - 커스텀훅 안쪽에서 자체적으로 빈 참조객체 생성
+	const refEl = useRef(null);
 	const [Frame, setFrame] = useState(null);
 	const scrollTo = targetPos => {
 		Frame && new Anime(Frame, { scroll: targetPos });
 	};
 
-	const getCurrentScroll = (selfEl, baseLine = 0) => {
+	const getCurrentScroll = (baseLine = 0) => {
 		const scroll = Frame.scrollTop - baseLine;
-		const modifiedScroll = scroll - selfEl?.offsetTop;
+		//순서5- 부모컴포넌트에서 참조객체 연결된 값을 hook내부적으로 활용
+		const modifiedScroll = scroll - refEl.current?.offsetTop;
 		return modifiedScroll;
 	};
 
@@ -17,5 +20,6 @@ export function useScroll() {
 		setFrame(document.querySelector('.wrap'));
 	}, []);
 
-	return { scrollTo, getCurrentScroll, Frame };
+	//순서2 - 부모에서 해당 참조객체를 활용하도록 리턴
+	return { scrollTo, getCurrentScroll, Frame, refEl };
 }
