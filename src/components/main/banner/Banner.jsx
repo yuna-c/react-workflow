05c90f25e7fe4c5306e1e7b965896f07ef220045
tubi55 <tubi55@nate.com>
2024-01-08 11:08:55 +0900

    바뀐 useScroll구문에 맞게 Pics컴포넌트 수정

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -5,13 +5,11 @@ import './Banner.scss';
 export default function Banner() {
 	const boxEl = useRef(null);
 
-	//순서1 - useScroll훅으로 전달한 해당 컴포넌트에서의 커스텀스크롤 함수 정의
 	const customHandleScroll = scroll => {
 		boxEl.current.style.transform = `rotate(${scroll / 2}deg) scale(${1 + scroll / 400})`;
 		boxEl.current.style.opacity = 1 - scroll / 400;
 	};
 
-	//순서2- 커스텀스크롤 함수를 useScroll호출시 인수로 전달
 	const { refEl } = useScroll(customHandleScroll);
 
 	return (
