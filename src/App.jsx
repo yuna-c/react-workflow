6cdcd65e88e63e500942db8e1a17bd7362fec88f
yuna <yuna744@gmail.com>
2024-01-12 11:15:24 +0900

    0112 App 컴포넌트 개발흐름

--- src/App.jsx
+++ src/App.jsx
@@ -75,10 +75,17 @@ export default function App() {
 }
 
 /*
-	1.reducer, store, app으로의 데이터 전달 흐름 이해
-	2.reducer가 하는 역할
-	3.action객체가 필요한 이유
-	4.컴포넌트에서 데이터호출 및 데이터변경 요청을 위한 useSelector, useDispatch
-	5.App에서 fetching후 action객체를 통해 dispatch로 reducer에 데이터 변경요청 흐름
-	6.action타입을 따로 객체형태로 관리하는 이유
+	App 컴포넌트 개발흐름
+	- 루트 컴포넌트이기 때문에 모든 비동기 데이터를 호출한뒤 액션객체를 생성해서 dispatch로 리듀서에 전달한 후 store에 저장
+
+	- 메인 페이지, 서브페이지용 컴포넌트를 Router설정
+	- 실무에서 아직 레거시코드를 많이 쓰기 때문에 일부러 react-router-dom, 같은 원리로 프로젝트에서 redux를 활용하고자 일부러 react17버전으로 작업
+	- module.scss는 아니지만 컴포넌트의 효율적인 유지보수를 위해서 폴더별로 jsx, scss파일을 그룹화하다보니 일반 미디어쿼리로 반응형처리하기에는 반복작업과 css변수활용에 어려움이 있어서 useMedia라는 커스텀훅을 직접 만들어서 스크립트로 반응형 스타일링되도록 처리
+	(useMedia 가이드 p.100)
+
+	해당 프로젝트 진행시 발생한 이슈사항들
+	1. 처음에 비동기데이터를 여러컴포넌트에서 재활용하기 위해서 redux를 사용했는데, Flickr데이터같이 검색이나 사용자 이벤트에 따라서 빈번하게 비동기데이터를 다시 담아야 되는 경우에는 redux로 처리하기 힘들었음
+
+	해결절차
+	- Flickr만 리덕스에서 제외하고 고민하던중에 비동기데이터 전역State 처리를 위한 redux-saga를 알게되고 saga버전으로 작업을 진행처리했다
 */
